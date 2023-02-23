5fc3316b405fbcbbc121e781eb66727dc990e870
zhijunlin7 <zlin1@cifpfbmoll.eu>
2023-02-23 17:40:24 +0100

    Haciendo Menu

--- app/src/main/java/com/example/a2048/MainActivity.java
+++ app/src/main/java/com/example/a2048/MainActivity.java
@@ -4,70 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.a2048.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button boton2048;
-    private Button botonlightout;
-    private ImageButton botonSiguiente;
-    private ImageButton BotonAnterior;
-    private String[] numColumnaYFila = {"3x3", "4x4", "5x5", "6x6"};
-    private TextView verDimension;
+    private ActivityMainBinding binding;
 
-    private int numPosicion = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        verDimension = (TextView) findViewById(R.id.verDimension);
-        verDimension.setText(numColumnaYFila[numPosicion]);
-
-        boton2048 = (Button) findViewById(R.id.comenzar);
-        boton2048.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Juego2048.class);
-                startActivity(intent);
-            }
-        });
-
-        botonlightout = (Button) findViewById(R.id.comenzar2);
-        botonlightout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Juegolightout.class);
-                startActivity(intent);
-            }
-        });
-
-        BotonAnterior = (ImageButton) findViewById(R.id.anterior);
-        BotonAnterior.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (numPosicion == 0) {
-                    numPosicion = numColumnaYFila.length - 1;
-                    verDimension.setText(numColumnaYFila[numPosicion]);
+    }
+    public class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (diffX > 0) {
+                        moverDerecha(numTextViews);
+                    } else {
+                        moverIzquierda(numTextViews);
+                    }
                 } else {
-                    numPosicion = (numPosicion - 1) % numColumnaYFila.length;
-                    verDimension.setText(numColumnaYFila[numPosicion]);
+                    if (diffY > 0) {
+                        moverAbajo(numTextViews);
+                    } else {
+                        moverArriba(numTextViews);
+                    }
                 }
+            } catch (Exception exception) {
+                exception.printStackTrace();
             }
-        });
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
 
-        botonSiguiente = (ImageButton) findViewById(R.id.siguiente);
-        botonSiguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                numPosicion = (numPosicion + 1) % numColumnaYFila.length;
-                verDimension.setText(numColumnaYFila[numPosicion]);
-
-            }
-        });
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
